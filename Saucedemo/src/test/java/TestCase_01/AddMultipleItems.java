40c69346a119ff0abc9dd22186c78da0052cfc98
Makay-Test <165439036+Makay-Test@users.noreply.github.com>
2024-12-12 07:01:29 +0800

    Add files via upload

---
+++ Saucedemo/src/test/java/TestCase_01/AddMultipleItems.java
@@ -0,0 +1,29 @@
+package TestCase_01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AddMultipleItems {
+
+	public static void main(String[] args) {
+
+		// Initialize webdriver and selenium.
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		// Login page
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+
+		// Add multiple items.
+		driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
+		driver.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")).click();
+	}
+}
---
+++ Saucedemo/src/test/java/TestCase_01/Login.java
@@ -0,0 +1,22 @@
+package TestCase_01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+
+	public static void main(String[] args) {
+
+		// Initialize webdriver and selenium dependencies.
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		// Login page
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		//input[@id='user-name']
+	}
+}
