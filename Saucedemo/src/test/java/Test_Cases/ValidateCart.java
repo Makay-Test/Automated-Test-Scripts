ab14c5d1414493f85a3271229777d5d23b144e3c
Makay-Test <165439036+Makay-Test@users.noreply.github.com>
2024-12-12 19:41:48 +0800

    Package_name_change
    Updated package name.

---
+++ Saucedemo/src/test/java/Test_Cases/ValidateCart.java
@@ -0,0 +1,40 @@
+package Test_Cases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ValidateCart {
+
+	public static void main(String[] args) {
+
+		// Initialize webdriver and selenium.
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		// Login page
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+
+		// Adding Items
+		driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+		// Validate if items are present in Cart
+		driver.findElement(By.id("shopping_cart_container")).click();
+
+		// Removing Items from cart
+		driver.findElement(By.id("remove-sauce-labs-bike-light")).click();
+		driver.findElement(By.id("remove-sauce-labs-backpack")).click();
+
+		// Proceeds to select other items
+		driver.findElement(By.id("continue-shopping")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+		driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+		// Validate if items are present in Cart
+		driver.findElement(By.id("shopping_cart_container")).click();
+	}
+}
