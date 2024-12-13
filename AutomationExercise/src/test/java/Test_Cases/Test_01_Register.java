77163c012819a36fd767c65efd093a8ab38858e0
Makay-Test <165439036+Makay-Test@users.noreply.github.com>
2024-12-13 21:03:39 +0800

    Update Test_01_Register.java

--- AutomationExercise/src/test/java/Test_Cases/Test_01_Register.java
+++ AutomationExercise/src/test/java/Test_Cases/Test_01_Register.java
@@ -10,12 +10,15 @@ public class Test_01_Register {
 
 	public static void main(String[] args) {
 
-		String InputClickXpathHolder[] = { "//input[@id='id_gender1']", "//option[text()='28']", "//option[text()='November']",
-				"//option[text()='1950']", "//input[@id='newsletter']", "//input[@id='optin']","//option[@value='Canada']"};
-		String InputTypeXpathHolder[] = {"//input[@id='password']","//input[@id='first_name']","//input[@id='last_name']","//input[@id='company']","//input[@id='address1']","//input[@id='state']","//input[@id='city']","//input[@id='zipcode']","//input[@id='mobile_number']",};
-		String InputTypeValueHolder[] = {"Testpass","TestName","LastTest","TestCompany","Blk 01 Lot 02 Test 03 Dr","Alberta","Red Deer","112233","2104104433"};
-		
-		
+		String InputClickXpathHolder[] = { "//input[@id='id_gender1']", "//option[text()='28']",
+				"//option[text()='November']", "//option[text()='1950']", "//input[@id='newsletter']",
+				"//input[@id='optin']", "//option[@value='Canada']" };
+		String InputTypeXpathHolder[] = { "//input[@id='password']", "//input[@id='first_name']",
+				"//input[@id='last_name']", "//input[@id='company']", "//input[@id='address1']", "//input[@id='state']",
+				"//input[@id='city']", "//input[@id='zipcode']", "//input[@id='mobile_number']", };
+		String InputTypeValueHolder[] = { "Testpass", "TestName", "LastTest", "TestCompany", "Blk 01 Lot 02 Test 03 Dr",
+				"Alberta", "Red Deer", "112233", "2104104433" };
+
 		// Initialize WebDriver
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
@@ -24,12 +27,6 @@ public class Test_01_Register {
 		driver.get("https://www.automationexercise.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		
-//		driver.findElement(By.xpath("")).click();
-//		driver.findElement(By.xpath("")).sendKeys("Testname");
-		
-		
-		
 		// Login
 		driver.findElement(By.xpath("//*[text()=' Signup / Login']")).click();
 
@@ -43,7 +40,12 @@ public class Test_01_Register {
 			driver.findElement(By.xpath(InputClickXpathHolder[a])).click();
 		}
 		for (int b = 0; b < 9; b++) {
-			driver.findElement(By.xpath(InputTypeXpathHolder[b])).sendKeys(InputTypeValueHolder[b]);}
+			driver.findElement(By.xpath(InputTypeXpathHolder[b])).sendKeys(InputTypeValueHolder[b]);
+		}
+//		driver.findElement(By.xpath("")).click();
+//		driver.findElement(By.xpath("")).sendKeys("Testname");
 	}
+
+
 
 }
